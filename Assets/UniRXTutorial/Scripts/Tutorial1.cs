8907beed6d0446e7c8f2047a45743917a9e6fd8f
Eric Kim <bosung90@gmail.com>
2017-01-27 01:02:00 -0800

    Tutorial1

---
+++ Assets/UniRXTutorial/Scripts/Tutorial1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UniRx;
+
+public class Tutorial1 : MonoBehaviour {
+
+	void Start () {
+        ReactiveProperty<int> B = new ReactiveProperty<int>(0);
+        ReactiveProperty<int> C = new ReactiveProperty<int>(0);
+        ReadOnlyReactiveProperty<int> A;
+
+        // A = B + C
+        A = Observable.CombineLatest(B, C, (b, c) => b + c).ToReadOnlyReactiveProperty();
+
+        // Prints 0
+        Debug.Log(A.Value);
+
+        B.Value = 5;
+        C.Value = 7;
+
+        // Pritns 12
+        Debug.Log(A.Value);
+    }
+}
